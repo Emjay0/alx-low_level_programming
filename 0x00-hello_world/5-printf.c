c52fbcb7875b623167c1b9ccfb75366ddc5d3188
ADISA ADEDOYIN <adisamujib11@gmail.com>
2022-09-10 05:57:14 -0700

    commit again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
 *main prints the string in the func
 *this programm prints with proper grammar
-*but the outcome is a piece of art 
+*but the outcome is a piece of art
 *Return:0
 **/
 int main(void)
