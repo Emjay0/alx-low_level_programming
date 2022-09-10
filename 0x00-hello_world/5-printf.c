bd30d23c98819621754dc055a1ffbee1de620360
ADISA ADEDOYIN <adisamujib11@gmail.com>
2022-09-10 05:52:44 -0700

    correction of 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main prints the string in the func
- *
- *
- * this programm prints with proper grammar, but the outcome is a piece of art and retuns 
- * Return:0
- **/
+* main prints the string in the func
+* this programm prints with proper grammar
+* but the outcome is a piece of art 
+* Return:0
+**/
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
