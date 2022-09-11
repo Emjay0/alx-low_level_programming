5783d622438a7747beab39b90016513aeaaa726a
ADISA ADEDOYIN <adisamujib11@gmail.com>
2022-09-11 01:30:04 -0700

    another commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-*main:print the string in the function
-*Description:using the main function
-*this programm prints with proper grammar
-*but the outcome is a piece of art
+*main - print the string in the function
+*
+*Description: using the main function
+*this programm prints Programming is like building a multilingual puzzle
 *Return:0
-**/
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
