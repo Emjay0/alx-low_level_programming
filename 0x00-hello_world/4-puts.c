3900061ea10d9619f9a51db195de0278c11d8846
ADISA ADEDOYIN <adisamujib11@gmail.com>
2022-09-11 01:55:03 -0700

    Use the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+*main - prints the string in the put function
+*
+*Description: using main function
+*this program prints programming is like building a multilingual puzzule
+*Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
