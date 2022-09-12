cfd796ea4543f6f53e8a10d1f09b7180b52d6572
ADISA ADEDOYIN <adisamujib11@gmail.com>
2022-09-12 09:30:24 -0700

    program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - print if the number is postive, zero, or negative
+*
+*Description: using the main function
+*this program prints the alphabet in lowercase, followed by a new line
+*Return: 0
+*/
+int main(void)
+{
+	char mj;
+	for (mj = 'a' ; mj <= 'z' ; mj++)
+	{
+		putchar(mj);
+	}
+	putchar('\n');
+	return (0);
+}
