18ee1ce1c7e1d6b7932f0db920d54bec453c2d84
ADISA ADEDOYIN <adisamujib11@gmail.com>
2022-09-22 02:01:13 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_strcat - concatena concatenates  the string pointed to by @src
+*@dest: String that will be appended
+*@src: String to be concatenated upon
+*
+*Return: returns poiner to @dest
+*/
+
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0;
+	dest_len = 0;
+	
+	while (dest[index++])
+	len++;
+	for (index = 0; src[index]; index++);
+	dest[dest_len++] = src[index];
+	return (dest);
+}
