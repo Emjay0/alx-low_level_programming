7a58099a2d6696930156a48ccb9b9236428f989e
ADISA ADEDOYIN <adisamujib11@gmail.com>
2022-09-22 02:21:51 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,23 @@
 *
 *Return: returns poiner to @dest
 */
-
 char *_strcat(char *dest, char *src)
 {
-	int index = 0; 
-	dest_len = 0;
+	int i;
+	int j;
 	
-	while (dest[index++])
-	dest_len++;
-	for (index = 0; src[index]; index++);
-	dest[dest_len++] = src[index];
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0');
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
