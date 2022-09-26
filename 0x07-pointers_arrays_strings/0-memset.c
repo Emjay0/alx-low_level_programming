b5478e8426d853f0b5c339a1b19502059bdb7065
emjay <adisamujib11@gmail.com>
2022-09-26 05:05:12 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_memset - fills the first byte of a memory with constant byte
+*@s:the address to memory block
+*@b: the character to be used
+*@n: the num of bytes to be used
+*
+*Return: returns pointer to mem block
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n-1] = b;
+		n --
+	}
+	return (s);
+}
