0dcdce5511b90b431aa8a9fa0c2d680b2073dc76
emjay <adisamujib11@gmail.com>
2022-09-27 09:47:39 -0700

    check

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 *_strspn - prints consec char os s1 in s2
@@ -11,12 +12,13 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, n, value, check;
+
 	value = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		check = 0;
-		
+
 		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (accept[n] == s[i])
